848d86f132d579472152788711c579313105cff4
AbiYasidAlMajid2300011Pilkom4A <abiyasid23@upi.edu>
2025-04-17 10:38:24 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,44 @@
 package com.abiyasidalmajid2300011.utspbo;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+        int luasAlas, luasSelubungLimas, tinggi;
+        
+        System.out.println("Limas 1");
         LimasSegiEmpat limasSegiEmpat1 = new LimasSegiEmpat();
         System.out.println("Limas segi empat dengan luas alas : " + limasSegiEmpat1.getLuasAlas() + ", luas selubung limas : " + limasSegiEmpat1.getLuasSelubungLimas() + " dan tinggi : " + limasSegiEmpat1.getTinggi() + ". Luasnya : " + limasSegiEmpat1.getLuas() + ", sedangkan volumenya : " + limasSegiEmpat1.getVolume());
         
-        LimasSegiEmpat limasSegiEmpat2 = new LimasSegiEmpat(30, 40, 50);
+        System.out.println("");
+        
+        System.out.println("Limas 2");
+        System.out.print("Masukkan luas alas: ");
+        luasAlas = myInput.nextInt();
+        System.out.print("Masukkan luas selubung limas: ");
+        luasSelubungLimas = myInput.nextInt();
+        System.out.print("Masukkan tinggi: ");
+        tinggi = myInput.nextInt();
+        
+        LimasSegiEmpat limasSegiEmpat2 = new LimasSegiEmpat(luasAlas, luasSelubungLimas, tinggi);
         System.out.println("Limas segi empat dengan luas alas : " + limasSegiEmpat2.getLuasAlas() + ", luas selubung limas : " + limasSegiEmpat2.getLuasSelubungLimas() + " dan tinggi : " + limasSegiEmpat2.getTinggi() + ". Luasnya : " + limasSegiEmpat2.getLuas() + ", sedangkan volumenya : " + limasSegiEmpat2.getVolume());
-    
+        
+        System.out.println("");
+        
+        System.out.println("Limas 3");
+        System.out.print("Masukkan luas alas: ");
+        luasAlas = myInput.nextInt();
+        System.out.print("Masukkan luas selubung limas: ");
+        luasSelubungLimas = myInput.nextInt();
+        System.out.print("Masukkan tinggi: ");
+        tinggi = myInput.nextInt();
+        
         LimasSegiEmpat limasSegiEmpat3 = new LimasSegiEmpat();
-        limasSegiEmpat3.setLuasAlas(25);
-        limasSegiEmpat3.setLuasSelubungLimas(35);
-        limasSegiEmpat3.setTinggi(45);
+        limasSegiEmpat3.setLuasAlas(luasAlas);
+        limasSegiEmpat3.setLuasSelubungLimas(luasSelubungLimas);
+        limasSegiEmpat3.setTinggi(tinggi);
         System.out.println("Limas segi empat dengan luas alas : " + limasSegiEmpat3.getLuasAlas() + ", luas selubung limas : " + limasSegiEmpat3.getLuasSelubungLimas() + " dan tinggi : " + limasSegiEmpat3.getTinggi() + ". Luasnya : " + limasSegiEmpat3.getLuas() + ", sedangkan volumenya : " + limasSegiEmpat3.getVolume());
     }    
 }
